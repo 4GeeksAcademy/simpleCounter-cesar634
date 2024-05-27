c0db5cbe7379f3c7c7fb257439cb3dddd17d4a9d
César Jovanni Zaldivar Camps <132796806+Cesarzaldivar@users.noreply.github.com>
2024-05-27 06:59:24 +0000

    contador de Cesar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = ({quinto,cuarto,tercero,segundo,primero}) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="blackZone container">
+      <div className="text-center text-light mt-5 bg-black d-flex justify-content-center">
+        <div className="m-1 fs-1 fw-bold bg-dark w-25">⏱︎</div>
+        <div className="m-1 fs-1 fw-bold bg-dark w-25">{quinto}</div>
+        <div className="m-1 fs-1 fw-bold bg-dark w-25">{cuarto}</div>
+        <div className="m-1 fs-1 fw-bold bg-dark w-25">{tercero}</div>
+        <div className="m-1 fs-1 fw-bold bg-dark w-25">{segundo}</div>
+        <div className="m-1 fs-1 fw-bold bg-dark w-25">{primero}</div>
+      </div>
+    </div>
 	);
+};
+
+Home.propTypes = {
+    quinto: PropTypes.number,
+    cuarto: PropTypes.number,
+    tercero: PropTypes.number,
+    segundo: PropTypes.number,
+    primero: PropTypes.number,
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,20 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let contador = 0;
+setInterval(()=>{
+    const cinco = Math.floor(contador / 10000) % 10;
+    const cuatro = Math.floor(contador / 1000) % 10;
+    const tres = Math.floor(contador / 100) % 10;
+    const dos = Math.floor(contador / 10) % 10;
+    const uno = Math.floor(contador / 1) % 10;
+    console.log(cinco, cuatro, tres, dos, uno)
+    ReactDOM.render(<Home 
+        quinto={cinco}
+        cuarto={cuatro}
+        tercero={tres}
+        segundo={dos}
+        primero={uno}
+    />, document.querySelector("#app"));
+    contador++;
+},1000)
